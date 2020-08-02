a5460f0b1596ae1ec36ee2b70ffe26162a2bf0cb
Sajid Ali <sajid_ali@elixir.com>
2020-08-02 12:14:52 +0500

    committing

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
-<?php
-    
-    // header('location:/install.php?shop=animationlistsforsale');
-    echo "THIS IS EXAMPLE APP";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>shopify App
+</body>
+</html>
